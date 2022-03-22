e639ec52e9519e41377c9a2bf9791c6de3c5f8ad
Roman <grromf@gmail.com>
2022-03-22 23:36:56 +0800

    added: method fadeToggle()

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 import './lib/lib';
 
 $('#first').on('click', () => {
-    $('div').eq(1).fadeOut(900);
+    $('div').eq(1).fadeToggle(900);
 });
 
 $('[data-count=second]').on('click', () => {
-    $('div').eq(2).fadeOut(900);
+    $('div').eq(2).fadeToggle(900);
 });
 
 $('.btn').eq(2).on('click', () => {
-    $('.w-500').fadeOut(900);
+    $('.w-500').fadeToggle(900);
 });
